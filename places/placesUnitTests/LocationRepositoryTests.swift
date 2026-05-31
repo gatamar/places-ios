22312d37688fb7efa89b8056725a9603c0ed10c4
gatamar <olia.pavliuk@gmail.com>
2026-05-31 23:58:40 +0200

    test-cover LocationRepo

---
+++ places/placesUnitTests/LocationRepositoryTests.swift
@@ -0,0 +1,52 @@
+//
+//  LocationRepositoryTests.swift
+//  places
+//
+//  Created by olia on 5/31/26.
+//
+
+import XCTest
+@testable import places
+
+final class LocationRepositoryTests: XCTestCase {
+    private var sut: LocationRepositoryImpl!
+    private var locationAPI: LocationsAPIMock!
+    override func setUp() {
+        super.setUp()
+        locationAPI = LocationsAPIMock()
+        sut = LocationRepositoryImpl(api: locationAPI)
+    }
+
+    func testFetchFromBackendSuccess() async {
+        locationAPI.stubbedResult = .success([.mock])
+        await sut.fetchFromBackend()
+        XCTAssertNil(sut.latestFetchError)
+        XCTAssertEqual(sut.locations.count, 1)
+    }
+    
+    func testFetchFromBackendFailure() async throws {
+        locationAPI.stubbedResult = .failure(.network(CancellationError()))
+        await sut.fetchFromBackend()
+        let error = try XCTUnwrap(sut.latestFetchError)
+        switch error {
+        case .decoding:
+            XCTFail("Expected network error")
+        case .network(let innerError):
+            XCTAssert(innerError is CancellationError)
+        }
+        XCTAssertEqual(sut.locations.count, 0)
+    }
+}
+
+private final class LocationsAPIMock: LocationsAPI {
+    var stubbedResult: Result<[LocationDTO], LocationFetchError> = .success([])
+    func fetch() async -> Result<[LocationDTO], LocationFetchError> {
+        stubbedResult
+    }
+}
+
+private extension LocationDTO {
+    static var mock: Self {
+        .init(name: "test", lat: 0, long: 0)
+    }
+}
